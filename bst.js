f8924656aaf7749ff101e0849218980ef6cb8ee3
rootankush <rootankushprompt@gmail.com>
2026-06-10 18:21:49 +0530

    fix: Fixed Binary Search Function

--- bst.js
+++ bst.js
@@ -7,15 +7,38 @@ class Node {
 }
 
 class Tree {
-	constructor(root) {
-		this.root = this.buildTree(array);
+	constructor(array) {
+		const sorted = this.sorted(array);
+		this.root = this.buildTree(sorted, 0, sorted.length - 1);
 	}
 
-	buildTree(array) {
+	sorted(array) {
 		const clean = [...new Set(array)];
 		const sorted = clean.sort((a, b) => a - b);
+		return sorted;
+	}
+
+	buildTree(array, start, end) {
 		if (start > end) return null;
 		const mid = Math.floor((start + end) / 2);
-		this.root = new Node(sorted[mid]);
+		const node = new Node(array[mid]);
+		node.left = this.buildTree(array, start, mid - 1);
+		node.right = this.buildTree(array, mid + 1, end);
+
+		return node;
+	}
+
+	prettyPrint(node = this.root, prefix = "", isLeft = true) {
+		if (node === null) return;
+
+		this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+
+		console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+
+		this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
 	}
 }
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 3, 5]);
+
+tree.prettyPrint();
